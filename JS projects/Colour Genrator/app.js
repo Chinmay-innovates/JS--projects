5361061a72a69150018d19abf7c762a58f3383ba
Chinmay-innovates <chinmaygope69@gmail.com>
2024-01-26 12:20:17 +0530

    add country guide

--- JS projects/Colour Genrator/app.js
+++ JS projects/Colour Genrator/app.js
@@ -1,25 +1,40 @@
 const btn = document.querySelector("#btn");
 const colorCode = document.querySelector("#color-code");
-const copyText= document.querySelector("#copyText");
+const copyText = document.querySelector("#copyText");
 // console.log(colorCode);
+var button = document.getElementById("copy-col");
+var hexCol = document.getElementById("hex-col");
 
-const copy =() =>{
-let copyText = colorCode;
+const copyToClipboard = (copy) => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+    return navigator.clipboard.writeText(copy);
+  }
+  return Promise.reject("The Clipboard API is not available");
+};
+copyToClipboard();
 
-color = navigator.clipboard.writeText(copyText.value);
-console.log(color);
-alert("color copied:  "+color.value);
-}
+button.onclick = function () {
+  navigator.clipboard.readText().then(function () {
+    const result = (document.getElementById(hexCol).innerText = getColor());
+    console.log(typeof result);
+    console.log(result);
+  navigator.clipboard.writeText(res.valueOf(result));
+  });
+};
+
+const copy = () => {
+  return navigator.clipboard.writeText(test);
+};
 const getColor = () => {
   //hex code
   const randNum = Math.floor(Math.random() * 16777215);
   const randCode = "#" + randNum.toString(16);
   //inserting hexcode
   document.body.style.backgroundColor = randCode;
-  colorCode.innerHTML =randCode;
-  console.log(randCode);
+  colorCode.innerHTML = randCode;
+  return randCode;
 };
 btn.addEventListener("click", getColor);
 
 //init call
-getColor();
+getColor(randCode);
