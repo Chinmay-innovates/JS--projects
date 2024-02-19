9940a47ef3ba562dcd6728a0fe8f0ea666fb08e5
Chinmay-innovates <chinmaygope69@gmail.com>
2024-02-19 22:26:51 +0530

    edit app.js

--- JS projects/3D  Sorting Visualizer/column.js
+++ JS projects/3D  Sorting Visualizer/column.js
@@ -5,6 +5,11 @@ class Column {
     this.height = height;
     this.width = width;
     this.queue = [];
+    this.color = {
+      r: 150,
+      g: 150,
+      b: 150,
+    };
   }
   // function to move to swapped location
   moveTo(loc, yOffset = 1, frameCount = 20) {
@@ -12,38 +17,45 @@ class Column {
       const t = i / frameCount;
       const u = Math.sin(t * Math.PI);
 
-      const lerp = (a, b, t) => {
-        return a + (b - a) * t;
-      };
-
       this.queue.push({
         x: lerp(this.x, loc.x, t),
         y: lerp(this.y, loc.y, t) + ((u * this.width) / 2) * yOffset,
+
+        r: lerp(150, 255, u),
+        g: lerp(150, 0, u),
+        b: lerp(150, 0, u),
       });
     }
   }
-
+  //jump methord
   jump(frameCount = 20) {
-    for (let i = 1; i <=frameCount; i++) {
+    for (let i = 1; i <= frameCount; i++) {
       const t = i / frameCount;
       const u = Math.sin(t * Math.PI);
 
       this.queue.push({
         x: this.x,
         y: this.y - u * this.width,
+
+        r: lerp(150, 0, u),
+        g: lerp(150, 0, u),
+        b: lerp(150, 0, u),
       });
     }
   }
+  //draw methord
   draw(ctx) {
     let changed = false;
     const left = this.x - this.width / 2;
     const top = this.y - this.height;
     const right = this.x + this.width / 2;
-    ctx.fillStyle = "rgb(150,150,150)";
+    const { r, g, b } = this.color;
+    ctx.fillStyle = `rgb(${r},${g},${b})`;
     if (this.queue.length > 0) {
-      const { x, y } = this.queue.shift();
+      const { x, y, r, g, b } = this.queue.shift();
       this.x = x;
       this.y = y;
+      this.color = { r, b, g };
       changed = true;
     }
 
@@ -76,3 +88,7 @@ class Column {
     return changed;
   }
 }
+
+const lerp = (a, b, t) => {
+  return a + (b - a) * t;
+};
